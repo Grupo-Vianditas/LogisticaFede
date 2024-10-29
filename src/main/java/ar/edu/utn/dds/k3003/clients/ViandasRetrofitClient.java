32f9c95d3664be359edf3c25791109d655c62eb6
fedehartwig <johndoe@example.com>
2024-10-29 20:11:38 -0300

    V3

--- src/main/java/ar/edu/utn/dds/k3003/clients/ViandasRetrofitClient.java
+++ src/main/java/ar/edu/utn/dds/k3003/clients/ViandasRetrofitClient.java
@@ -15,7 +15,7 @@ public interface ViandasRetrofitClient {
   @PATCH("viandas/{qr}/estado")
   Call<ViandaDTO> modificarEstado(
           @Path("qr") String qr,
-          @Query("status") EstadoViandaEnum estado
+          @Query("estado") EstadoViandaEnum estado
   );
 
 }
